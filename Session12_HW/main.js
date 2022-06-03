1206ccbd15920f5fd49186edcb8246164aa1e357
notaonce <chsj1009@naver.com>
2022-06-03 20:06:49 +0900

    session12hw

---
+++ Session12_HW/main.js
@@ -0,0 +1,22 @@
+play = (e) => {
+  console.log(e);
+    const audio = document.querySelector(`audio[data-press="${e.keyCode}"]`);
+    const key = document.querySelector(`li[data-press="${e.keyCode}"]`);
+  if(audio) {
+    audio.play();
+    key.classList.add("play");
+  }
+}
+
+pause = (e)=>{
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`li[data-key = "${e.keyCode}"]`);
+  if(audio){
+    audio.currentTime = 0;
+    audio.pause();
+    key.classList.remove("play");
+  }
+}
+
+window.addEventListener("keypress", play);
+window.addEventListener("keyup", pause);
